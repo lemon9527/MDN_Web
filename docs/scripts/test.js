93be8a3565f1566beaedee1354390a58b1818ef1
lemon9527 <lliu3@fellowes.com>
2026-05-05 19:31:15 +0800

    update

---
+++ docs/scripts/test.js
@@ -0,0 +1,6 @@
+const noWhitespace = 
+document.getElementById("noWhitespace").innerHTML;
+console.log(noWhitespace);
+
+const Whitespace = document.getElementById("Whitespace").innerHTML;
+console.log(Whitespace);
